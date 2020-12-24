46758ca23ff5234504cf73d14fae852380a983db
Danzzel <75837219+Danzzel@users.noreply.github.com>
2020-12-24 14:45:54 +0200

    Робоча галерея

--- js/main.js
+++ js/main.js
@@ -14,3 +14,36 @@ sitebarToggleBtn.onclick = function () {
     menuIcon.classList.toggle('menuIconActive');
     mobNavigationActive.classList.toggle('mobNavigationActive');
 };
+
+var btnGalleryPrev = document.querySelector('.btnGalleryPrev')
+var btnGalleryNext = document.querySelector('.btnGalleryNext')
+var galeryImg = document.querySelector('.galeryImg')
+var i =1
+
+var galery = [
+    '../img/Layer20.png',
+    '../img/Layer21.png',
+    '../img/Layer22.png'
+    
+]
+
+
+
+btnGalleryNext.onclick = function () {
+    if(i>=2){
+        i=0
+    }
+    else{i++}
+    
+    galeryImg.src = galery[i]
+}
+btnGalleryPrev.onclick = function () {
+    if(i<=0){
+    i=2
+    }
+else {i--}
+    
+    galeryImg.src = galery[i]
+  
+} 
+ 
