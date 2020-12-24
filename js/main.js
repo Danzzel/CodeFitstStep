755cafa927f99b6523a8314700b4b16bc1f72d1b
Danzzel <75837219+Danzzel@users.noreply.github.com>
2020-12-24 13:48:32 +0200

    1

--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@ const block = document.querySelector('.block')
 const happy = document.querySelector('.happy')
 const sad = document.querySelector('.sad')
 
+var input = document.querySelector('.input')
+var inputBtn = document.querySelector('.inputBtn')
+var yourText = document.querySelector('.yourText')
+
 var hasHappy = block.classList.contains ('happy')
 // var hasSad = block.classList.contains ('sad')
-$(block).on('click', function(){
+ function changeSmile(){
 
     if (hasHappy) {
         block.classList.remove ('happy')
@@ -15,4 +19,26 @@ $(block).on('click', function(){
         block.classList.add ('happy')
     }
 
+}
+
+block.addEventListener('click', changeSmile)
+
+
+function castomText () {
+    yourText.textContent = input.value
+}
+inputBtn.addEventListener('click',castomText)
+
+yourText.addEventListener('mouseenter', function(){
+    this.style.color = 'red';
+})
+yourText.addEventListener('mouseleave', function(){
+    this.style.color = 'black';
+})
+
+block.addEventListener('mouseenter',function(){
+    this.style.backgroundImage = 'url(../img/imgExperiments/HappySmiley.png'
+})
+block.addEventListener('mouseleave',function(){
+    this.style.backgroundImage = 'url(../img/imgExperiments/sadSmile.png'
 })
